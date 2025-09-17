e92c15a1461a96d9d729c9aa63bac913e23eb1cf
Aren2025 <arencroos@gmail.com>
2025-09-17 12:54:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -498,8 +498,9 @@ def admin_precios():
 
 # ----------------- Iniciar app -----------------
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port, debug=False)
+
 
 
 
